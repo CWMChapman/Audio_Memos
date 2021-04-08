263085ab4ccffc225b485f526968aad171e7c587
Christopher Chapman <cwmchapman@gmail.com>
2021-04-08 11:06:55 -0400

    few implementations of media recorder

--- app/src/main/java/edu/fandm/cchapman/audiomemos/MainActivity.java
+++ app/src/main/java/edu/fandm/cchapman/audiomemos/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.Context;
 import android.graphics.Color;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +24,11 @@ public class MainActivity extends AppCompatActivity {
     // OVERVIEW: https://developer.android.com/guide/topics/media/mediarecorder
 
     private static final String LOG_TAG = "AudioRecordTest";
+    private static final String DEFAULT_TAG = "Default Tag";
     public static boolean isRecording = false;
 
 
-//    MediaRecorder mr;
+    MediaRecorder mr;
 
 
     @Override
@@ -40,23 +45,42 @@ public class MainActivity extends AppCompatActivity {
         vibrate();
         Button b = findViewById(R.id.record_button);
         if (isRecording == false) {
-            beginRecording(b);
+            startRecording(b);
         } else {
             stopRecording(b);
         }
         isRecording = !isRecording; // toggle recording status
     }
 
-    public void beginRecording(Button b) {
+    public void startRecording(Button b) {
         Log.d(LOG_TAG, "Switching to recording mode.");
         b.setBackgroundColor(Color.RED);
         b.setText("Recording...");
+
+        File storageDirectory = Environment.getExternalStorageDirectory();
+        File recordingFile;
+        try {
+            recordingFile = File.createTempFile("sound", ".3gp", storageDirectory);
+        } catch (IOException e) {
+            Log.e(DEFAULT_TAG, "external storage access error");
+            return;
+        }
+
+        mr = new MediaRecorder();
+        mr.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mr.setOutputFile(recordingFile.getAbsolutePath());
     }
 
     public void stopRecording(Button b) {
         Log.d(LOG_TAG, "Finishing recording.");
         b.setBackgroundColor(Color.GRAY);
         b.setText("Record");
+
+        mr.stop();
+        mr.release();
+        // now we need to list the file on the screen
     }
 
 
